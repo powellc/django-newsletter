0403682ae160ecf15bd8a75a76c9be8e6b576ea7
Colin Powell <colin@onecardinal.com>
2011-04-20 23:58:42 -0400

    Moving to class based views.

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,11 +1,13 @@
+import logging
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from django.http import *
+from django import http
+from django.views.generic.list import BaseListView
+from django.views.generic import DetailView
+from django.views.generic.edit import FormView
 from django.conf import settings
-from django.template.loader import render_to_string
-from django.db.models import get_model
 
-from newsletter.models import Subscription
+from newsletter.models import Subscription, Edition, Newsletter
 from newsletter.forms import SubscriptionForm
 from newsletter.core import csv
 
@@ -13,37 +15,47 @@ import datetime
 import re
 
 from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 
-@staff_member_required
-def generate_csv(request, model_str="newsletter.subscription", data=None):
-    '''
-    TODO:
+class ExcelResponseMixin(object):
+    def render_to_response(self, context):
+        return self.get_csv_response(self.convert_context_to_xls(context))
+
+    def get_csv_response(self, content, **httpresponse_kwargs):
+        return http.HttpResponse(content['data'], 
+                                 content_type=content['mimetype'],
+                                 **httpresponse_kwargs)
+
+    def convert_context_to_xls(self, context):
+        return csv.generate_csv(context)
+
+class ExcelView(ExcelResponseMixin, BaseListView):
+    model = Subscription
+
+class DefaultNewsletterView(DetailView, FormView):
+    template_name = 'newsletter/newsletter_detail.html'
+    form_class = SubscriptionForm
+    success_url = '/'
     
-    '''
+    def get_object(self):
+        return Newsletter.objects.get(default=True)
 
-    if not data:
-        model = get_model(*model_str.split('.'))
-        data = model._default_manager.filter(subscribed=True)
-    
-    if len(data) == 0:
-        data = [["no subscriptions"],]
-    return csv.ExcelResponse(data)
+    def get_context_data(self, **kwargs):
+        context = super(DefaultNewsletterView, self).get_context_data(**kwargs)
+        context['form'] = SubscriptionForm
+        return context
 
-def subscribe_detail(request, form_class=SubscriptionForm, 
-        template_name='newsletter/subscribe.html',  
-        success_template='newsletter/success.html', extra_context={}, 
-        model_str="newsletter.subscription"):
+class NewsletterDetailView(DetailView, FormView):
+    model = Newsletter
+    form_class = SubscriptionForm
 
-    if request.POST:   
-        try:
-            model = get_model(*model_str.split('.')) 
-            instance = model._default_manager.get(email=request.POST['email'])
-        except model.DoesNotExist: 
-            instance = None
-        form = form_class(request.POST, instance = instance)
+    def get_context_data(self):
+        context['form'] = SubscriptionForm
+        return context
+
+    def post(request, *args, **kwargs):
         if form.is_valid():
             subscribed = form.cleaned_data["subscribed"]
-            
             form.save()
             if subscribed:
                 message = getattr(settings,
@@ -51,7 +63,7 @@ def subscribe_detail(request, form_class=SubscriptionForm,
             else:
                 message = getattr(settings,
                      "NEWSLETTER_OPTOUT_MESSAGE", 
-                     "You've been removed. Sorry to see ya go.")          
+                     "You've been removed. Sorry to see ya go.")
 
             extra = {
                 'success': True,
@@ -59,16 +71,5 @@ def subscribe_detail(request, form_class=SubscriptionForm,
                 'form': form_class(),
             }
             extra.update(extra_context)
-            
             return render_to_response(success_template, extra, 
                  RequestContext(request))
-    else:
-        form = form_class()
-    
-    extra = {
-        'form': form,
-    }
-    extra.update(extra_context)
-    
-    return render_to_response(template_name, extra, RequestContext(request))
-

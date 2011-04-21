9fe5cb230d3b647e9d99f25ba6d2074ea8cf0e83
Colin Powell <colin@onecardinal.com>
2011-04-20 23:59:18 -0400

    Making the app a bit more robust.

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -3,18 +3,19 @@ from django.contrib import admin
 from django.views.generic.simple import direct_to_template
 from django.conf import settings
 
+from newsletter.models import Edition
+from newsletter.views import ExcelView, NewsletterDetailView, DefaultNewsletterView
+
 admin.autodiscover()
 
-urlpatterns = patterns('newsletter.views',
+urlpatterns = patterns('',
 
     url (r'^admin/newsletter/subscription/download/csv/$', 
-        view='generate_csv',
+        view=ExcelView.as_view(),
         name='download_csv',
     ),
     
-    url (r'^$', 
-        view='subscribe_detail',
-        name='subscribe_detail',
-    ),
-
+    #url (r'subscribe/$', view=views.subscribe_detail, name='subscribe_detail', ),
+    url (r'(?P<slug>[-\w]+)/$', view=NewsletterDetailView.as_view(), name='nw-newsletter-detail', ),
+    url (r'^$', view=DefaultNewsletterView.as_view(), name='nw-index', ),
 )
